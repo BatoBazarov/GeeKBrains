ea9eac8b76fbda9dae581a9fc80853d573ddc5e9
BatoBazarov <bato1806@mail.ru>
2022-06-03 15:52:49 +0300

    Initial commit

---
+++ src/lesson1/Seamstress.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class Seamstress {
+
+    private String name;
+    private int sewingSpeed;
+
+    public Seamstress(String name, int sewingSpeed) {
+        this.name = name;
+        this.sewingSpeed = sewingSpeed;
+    }
+    public String getName() { return name; }
+
+    public int getSewingSpeed() { return sewingSpeed; }
+}
+
---
+++ src/lesson1/Team.java
@@ -0,0 +1,13 @@
+package lesson1;
+public class Team {
+
+    private String name;
+    private Seamstress[] teams;
+    public Team(String name, Seamstress[] teams) {
+        this.name = name;
+        this.teams = teams;
+    }
+    public String getName() { return name; }
+
+    public Seamstress[] getTeams() { return teams;}
+}
